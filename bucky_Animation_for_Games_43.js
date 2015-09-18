29638d73112f18b3d1fb2a34aab912e8f17ae1d6
unknown <vf7714@46F1BS1.dupontnet.net>
2015-09-18 17:20:07 -0400

    43

---
+++ bucky_Animation_for_Games_43.js
@@ -0,0 +1,15 @@
+function doFirst(){
+	var x = document.getElementById('canvas');
+	canvas = x.getContext('2d');
+	
+	window.addEventListener("mousemove", bucky, false);	
+}
+
+function bucky(e){
+	canvas.clearRect(0,0,600,400); //clear all pixels within this shape
+	var xPos = e.clientX;
+	var yPos = e.clientY;
+	canvas.fillRect(xPos-50,yPos-50, 100, 100);
+}
+
+window.addEventListener("load", doFirst, false);
